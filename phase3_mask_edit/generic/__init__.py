d8c317784a37bc790e8035dd7434ca537d951f83
carrelachi-coder <carrelachi@gmail.com>
2026-05-06 09:49:17 +0800

    fix

--- phase3_mask_edit/generic/__init__.py
+++ phase3_mask_edit/generic/__init__.py
@@ -1,6 +1,11 @@
 """Generic mask editing strategies that must work on all six datasets."""
 
 from phase3_mask_edit.generic.boundary import apply_boundary_pushing_remodel
+from phase3_mask_edit.generic.executor import (
+    EditExecutionResult,
+    execute_edit,
+    register_primitive,
+)
 from phase3_mask_edit.generic.tumor_burden import (
     PrimitiveEditResult,
     PrimitiveExecutionError,
@@ -9,9 +14,12 @@ from phase3_mask_edit.generic.tumor_burden import (
 )
 
 __all__ = [
+    "EditExecutionResult",
     "PrimitiveEditResult",
     "PrimitiveExecutionError",
     "apply_boundary_pushing_remodel",
     "apply_tumor_burden_decrease",
     "apply_tumor_burden_increase",
+    "execute_edit",
+    "register_primitive",
 ]
